d50f7eb91256bd5bee6b4f44c4ce50bd036b3c69
Vanshika190 <vanshikagahlot561@gmail.com>
2024-06-25 20:06:04 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [todo, settodo] = useState([]);
+  
+
+  let saveTodoList = (event) => {
+    let todoName = event.target.todoName.value;
+    if (!todo.includes(todoName)) {
+      let finalTodo = [...todo, todoName];
+      settodo(finalTodo);
+    } else {
+      alert("ToDo Already Exist");
+    }
+
+    event.preventDefault();
+  };
+  let list = todo.map((value, index) => {
+    return(
+      <TodoListItems value={value} key={index} indexno={index} 
+      todo={todo} 
+      settodo={settodo} />
+    )
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cont">
+        <h1>To-Do List</h1>
+        <form onSubmit={saveTodoList}>
+          <input type="text" name="todoName" placeholder="Add Task" />{" "}
+          <button>Save</button>
+        </form>
+        <div className="todoItems">
+          <ul>
+            {list}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function TodoListItems({value,indexno,todo,settodo}) {
+
+  let [Tstatus,setTstatus]=useState(false);
+
+  let deleteRow=()=>{
+    let finalData=todo.filter((v,i)=> i!=indexno);
+    settodo(finalData);
+  }
+    let checkStatus=()=>{
+      setTstatus(!Tstatus);
+    }
+
+
+  return (
+    <li className={(Tstatus)?'todocheck' : ''} onClick={checkStatus}>
+      {value}
+      <span onClick={deleteRow}>
+      <i class="fa-solid fa-trash"></i>
+      </span>
+    </li>
+  )
+}
